ae6531b6b5ef7c318acb110fca616a8adf7d6163
Fr1k1 <martin.friscic6@gmail.com>
2023-02-22 22:45:47 +0100

    Implemented logic...css remaining

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("spojeno");
+const input=document.getElementById("input_field");
+
+
+document.querySelector("#add_button").addEventListener("click", function() {
+    const newDiv = document.createElement('div');
+    newDiv.innerText = input.value;
+    document.body.appendChild(newDiv).classList.add("new_item");
+    input.value = '';
+})
+
+
+document.querySelector("#remove_button").addEventListener("click", function() {
+    const divs = document.querySelectorAll('div.new_item');
+    divs.forEach(function(div) {
+        div.remove();
+      });
+
+})
