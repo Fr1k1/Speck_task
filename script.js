f3a8b3998403ac23fc6e46443eed1f907285d5a4
Fr1k1 <martin.friscic6@gmail.com>
2023-02-22 23:31:38 +0100

    Made code more intuitive

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const input=document.getElementById("input_field");
+const input = document.getElementById("input_field");
 
 
-document.querySelector("#add_button").addEventListener("click", function() {
-    const newDiv = document.createElement('div');
-    newDiv.innerText = input.value;
-    document.body.appendChild(newDiv).classList.add("new_item");
+document.querySelector("#add_button").addEventListener("click", function () {
+    const newTechnology = document.createElement('div');
+    newTechnology.innerText = input.value;
+    document.body.appendChild(newTechnology).classList.add("new_item");
     input.value = '';
 })
 
 
-document.querySelector("#remove_button").addEventListener("click", function() {
-    const divs = document.querySelectorAll('div.new_item');
-    divs.forEach(function(div) {
-        div.remove();
-      });
+document.querySelector("#remove_button").addEventListener("click", function () {
+    const divs = document.querySelectorAll('div.new_item'); //get all divs that have class new_item
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].remove();
+    }
 
 })
